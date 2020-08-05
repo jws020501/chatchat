85a66347006a94d061a50a4c6b533292016f0604
wonseok <jws020501@naver.com>
2020-08-05 16:16:08 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from "expo";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+const io = require("socket.io-client");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Replace this URL with your own, if you want to run the backend locally!
+const SocketEndpoint = "https://server0501.herokuapp.com/";
+
+export default class App extends React.Component {
+  state = {
+    isConnected: false,
+    data: null,
+  };
+  componentDidMount() {
+    const socket = io(SocketEndpoint, {
+      transports: ["websocket"],
+    });
+
+    socket.on("connect", () => {
+      this.setState({ isConnected: true });
+    });
+    socket.on("connection", () => {
+      this.setState({ isConnected: true });
+    });
+    var name = "김만덕";
+    socket.emit("asd", name);
+  }
+
+  render() {
+    return <View style={styles.container}></View>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 170,
+    borderColor: "gray",
+  },
+  sendbtn: {
+    height: 40,
+    width: 45,
+    backgroundColor: "blue",
+  },
+  textbar: {
+    height: 45,
   },
 });
